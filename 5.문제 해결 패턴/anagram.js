c8153d6b8497de705220df242c792ea8b9fecf0f
lee <vawav11@gmail.com>
2022-05-22 00:19:21 +0900

    220522

---
+++ 5.문제 해결 패턴/anagram.js
@@ -0,0 +1,31 @@
+// 아나그램 알고리즘
+
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // letter가 존재하면 1을 더하고 아니라면 1로 초기화
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+  console.log(lookup);
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // letter가 0일때 if문이 실행되면 false를 반환, 아니라면 1을 뺀다
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
+
+// {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+validAnagram("anagrams", "nagaramm");
